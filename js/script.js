5c25878fe1cf5ce2290e6ec967c2a5ceeaa827ea
Yasmim Moraes <96023730+yasmimmoraes@users.noreply.github.com>
2022-01-15 15:36:03 -0300

    changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function toggleVideo() {
     const trailer = document.querySelector('.trailer');
-    const video = document.querySelector('Video');
+    const video = document.querySelector('video');
     trailer.classList.toggle('active');
 
     video.currentTime = 0;
